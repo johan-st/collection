c2ce4779f1444bc6a56f10309c92995b1197708d
Jay <53336235+johan-STYL@users.noreply.github.com>
2020-09-25 12:50:12 +0200

    .

--- src/Page/RomanNumerals.elm
+++ src/Page/RomanNumerals.elm
@@ -1,1 +1,31 @@
-module Pages.RomanNumerals exposing (..)
+module Page.RomanNumerals exposing (..)
+
+
+type Numeral
+    = I
+    | V
+    | X
+    | L
+    | C
+    | D
+    | M
+
+
+toRoman : Int -> List Numeral
+toRoman num =
+    fn num []
+
+
+fn : Int -> List Numeral -> List Numeral
+fn x r =
+    case x of
+        0 ->
+            r
+
+        _ ->
+            if x // 5 > 0 then
+                fn (x - 5) (V :: r)
+            else if x // 4 > 0 then
+                fn (x - 4) (I :: V :: r)
+            else
+                fn (x - 1) (I :: r)
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,6 +3,7 @@ module Tests exposing (..)
 import Expect
 import Fuzz exposing (int, intRange)
 import Page.FizzBuzz exposing (Soda(..), carbonate)
+import Page.RomanNumerals exposing (Numeral(..), toRoman)
 import Test exposing (..)
 
 
@@ -40,5 +41,14 @@ all =
                 \int ->
                     carbonate int
                         |> Expect.equal (Uncarbonated int)
+            ]
+        , describe "roman numerals"
+            [ test "1 -> [I]" <| \_ -> toRoman 1 |> Expect.equal [ I ]
+            , test "3 -> [III]" <| \_ -> toRoman 3 |> Expect.equal [ I, I, I ]
+            , test "4 -> [IV]" <| \_ -> toRoman 4 |> Expect.equal [ I, V ]
+            , test "5 -> [V]" <| \_ -> toRoman 5 |> Expect.equal [ V ]
+            , test "8 -> [VIII]" <| \_ -> toRoman 8 |> Expect.equal [ V, I, I, I ]
+            , test "9 -> [IX]" <| \_ -> toRoman 9 |> Expect.equal [ I, X ]
+            , test "10 -> [X]" <| \_ -> toRoman 10 |> Expect.equal [ X ]
             ]
         ]
