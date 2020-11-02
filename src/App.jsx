6995b79bef3944fc11004466e5882f5ce32fc50a
Jay <53336235+johan-STYL@users.noreply.github.com>
2020-11-02 20:08:35 +0100

    start setting things up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Gallery from './Components/Gallery';
+import Landing from './Components/Landing';
+import Katas from './Components/Katas';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Components/Katas.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Katas() {
+  return <div>Katas</div>;
+}
+
+export default Katas;
