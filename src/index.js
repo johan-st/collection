be8eaf62e721921ccce7524a1301f8a37d24c9bc
Jay <53336235+johan-STYL@users.noreply.github.com>
2020-11-02 19:57:35 +0100

    Turnes out its a remake

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
-import "./main.css";
-import { Elm } from "./Main.elm";
-import * as serviceWorker from "./serviceWorker";
-import { initSound } from "./sound";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Extract the stored data from previous sessions.
-var storedData = localStorage.getItem("elm-model");
-var flags = storedData ? JSON.parse(storedData) : "null";
-const sounds = initSound();
-const app = Elm.Main.init({
-  node: document.getElementById("root"),
-  flags,
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-app.ports.log.subscribe((log) => console.log("elm-log-port:\n", log));
-
-// Listen for commands from the `setStorage` port.
-// Turn the data to a string and put it in localStorage.
-app.ports.setPersist.subscribe((persist) => {
-  // console.log("setPersist\n", JSON.parse(persist);
-  localStorage.setItem("elm-model", JSON.stringify(persist));
-});
-// playSound;
-app.ports.soundPortActual.subscribe((select) => {
-  if (select === "click") {
-    sounds.click.play();
-  } else if (select === "coin") {
-    sounds.coin.play();
-  } else if (select === "jingle") {
-    sounds.jingle.play();
-  } else if (select === "correct") {
-    sounds.correct.play();
-  } else {
-    console.log("bad sound selection:", select);
-  }
-});
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/sound.js
+++
@@ -1,8 +0,0 @@
-export function initSound() {
-  return {
-    click: new Audio("/static/click.wav"),
-    coin: new Audio("/static/coin.wav"),
-    correct: new Audio("/static/correct8bit.wav"),
-    jingle: new Audio("/static/jingle.wav"),
-  };
-}
