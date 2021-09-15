518277672d41d7ae86b17a0eac2e9dca27a377d2
Johan Strand <53336235+johan-st@users.noreply.github.com>
2021-09-15 19:04:56 +0200

    fix broken test

--- tests/HelperTest.elm
+++ tests/HelperTest.elm
@@ -1,26 +1,40 @@
 module HelperTest exposing (..)
 
 import Array
-import Expect exposing (equal)
-import Fuzz exposing (int, intRange)
-import Page.Gallery exposing (Card, Image(..), toggleCard)
+import Expect
+import Page.Gallery exposing (Card, toggleCard)
 import Test exposing (..)
 
 
 helpers : Test
 helpers =
+    let
+        image =
+            { id = "id"
+            , description = Nothing
+            , alt = Nothing
+            , url = "url"
+            , likes = 42
+            , user =
+                { name = "user.name"
+                , location = Nothing
+                , bio = Nothing
+                }
+            }
+    in
     describe "Gallery"
         [ test "card selected toggle" <|
             \_ ->
                 let
                     cards =
-                        Array.fromList [ Card None False, Card None False, Card None True ]
+                        Array.fromList [ Card image False, Card image False, Card image False ]
 
                     expectedResult =
-                        Array.fromList [ Card None True, Card None True, Card None False ]
+                        Array.fromList [ Card image True, Card image False, Card image False ]
                 in
                 cards
                     |> toggleCard 1
+                    |> toggleCard 2
                     |> toggleCard 2
                     |> toggleCard 0
                     |> Expect.equal expectedResult
